d25dda1a9c5227cc796129f14f134b0ca56326c4
Purity Mwaura <purity.mwaura@student.moringaschool.com>
2022-06-12 10:14:39 -0400

    commit tree f2eb58506d7fe5ad1dd44defa7936f714cebbd4b

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+// Write your code here!
+const main = document.querySelector('main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = "Michael is the champion";
+
+document.body.append(newHeader);
